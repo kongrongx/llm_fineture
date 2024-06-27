5a17873cb1f2a2ef9a15cc76185c28ba81234d94
kongrong <kongrongx@gmail.com>
2024-06-27 19:25:28 +0800

    首次提交

---
+++ chatglm_6b_finetune/merge_lora_basemodel.py
@@ -0,0 +1,29 @@
+from peft import PeftModel, LoraConfig
+from transformers import AutoTokenizer, AutoModel
+
+# 加载已经微调过的LoRA模型
+base_model_path = 'G:/WorkSpace/aigc/llm_models_store/llm_chat_models/chatglm3_6b'
+lora_model_path = 'output/checkpoint-30'
+
+base_model_tokenizer = AutoTokenizer.from_pretrained(base_model_path, trust_remote_code=True)
+base_model = AutoModel.from_pretrained(
+    base_model_path,
+    trust_remote_code=True,
+)
+
+
+model = PeftModel.from_pretrained(base_model, lora_model_path)
+
+
+
+# 合并LoRA权重到基础模型中
+merged_model = model.merge_and_unload()
+
+# 保存合并后的模型
+output_path = 'G:/WorkSpace/aigc/llm_models_store/llm_chat_models/chatglm3_6b_lore_self_condition'
+merged_model.save_pretrained(output_path)
+
+
+base_model_tokenizer.save_pretrained(output_path)
+
+
